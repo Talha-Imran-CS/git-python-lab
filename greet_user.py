af2e2b3ec3baf65824ff250da0a3468f9830a4c1
Talha Imran <timran.bscs25seecs@seecs.edu.pk>
2025-12-05 15:36:20 +0500

    Add three python function files

---
+++ greet_user.py
@@ -0,0 +1,4 @@
+def greet(name): 
+    prnt(f"Hello, {name}!")
+if __name__ = "__main__":
+     greet("Talha")
---
+++ multiply.py
@@ -0,0 +1,4 @@
+def multiply(a, b):
+return a*b
+if __name__ == "__main__":
+print("product:", multiply(5,3))
